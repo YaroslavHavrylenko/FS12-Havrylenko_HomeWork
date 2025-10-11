132c7463c7074b63de36182b002a3602d999723d
YaroslavHavrylenko <yasx80@gmail.com>
2025-10-11 10:44:50 +0300

    HW1

--- src/main/java/HW1/Homework1.java
+++ src/main/java/HW1/Homework1.java
@@ -2,6 +2,9 @@ package HW1;
 
 public class Homework1 {
     public static void main(String[] args) {
-        
+        byte b = 3;
+        short s = 124;
+        int i = 3214243;
+        long l = 3423425;
     }
 }
