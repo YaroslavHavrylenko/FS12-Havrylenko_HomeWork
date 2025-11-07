3eec04a457bf18b6bb17b689db722a85570082a2
YaroslavHavrylenko <yasx80@gmail.com>
2025-11-07 09:30:17 +0200

    HW7 - HappyFamily - Start

---
+++ src/main/java/HW7/human/Man.java
@@ -0,0 +1,27 @@
+package HW7.human;
+
+public final class Man extends Human {
+    public Man(String name, String surname, int age) {
+        super(name, surname, age);
+    }
+
+    public Man(String name, String surname, int age, int iq) {
+        super(name, surname, age, iq);
+    }
+
+    public Man(String name, String surname, int age, int iq, String[][] schedule) {
+        super(name, surname, age, iq, schedule);
+    }
+
+    public Man() {
+    }
+
+    @Override
+    public void greetPet() {
+        System.out.printf("Hi, %s! You are too witty!\n", this.getFamily().getPet().getNickname());
+    }
+
+    void repair () {
+        System.out.println("My car is ready to run again!");
+    }
+}
---
+++ src/main/java/HW7/pet/Fish.java
@@ -0,0 +1,40 @@
+package HW7.pet;
+
+import java.util.Arrays;
+
+public class Fish extends Pet{
+    private Species species;
+
+    public Fish (Species specie, String nickname) {
+        super(nickname);
+        this.species = Species.FISH;
+    }
+
+    public Fish (Species specie, String nickname, int age, int trickLevel, String[] habits) {
+        super(nickname, age, trickLevel, habits);
+        this.species = Species.FISH;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    @Override
+    public void respond() {
+        System.out.println("Bol! Bol! Bol!");
+    }
+
+    @Override
+    public String toString() {
+        return this.getSpecies() + "{" +
+                "nickname='" + this.getNickname() + '\'' +
+                (this.getAge() == 0 ? "" : ", age=" + this.getAge()) +
+                (this.getTrickLevel() == 0 ? "" : ", trickLevel=" + this.getTrickLevel()) +
+                (this.getHabits() == null ? "" : ", habits=" + Arrays.toString(this.getHabits())) +
+                '}';
+    }
+}
